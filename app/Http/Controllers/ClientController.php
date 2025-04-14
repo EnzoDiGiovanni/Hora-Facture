e4bebff65bf708294e596c3c05cd791f1eb16ce8
Enzo <digiovannienzo1@gmail.com>
2025-04-14 16:48:54 +0200

    feat(auth): add ClientController

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $clients = Client::all();
+        return view('client.index', compact('clients'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('client.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'bussiness_name' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+            'ical_url' => 'required|string|max:255',
+        ]);
+
+        Client::create($validated);
+
+        return redirect()->route('clients.index')->with('success', 'Client créé avec succès.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Client $client)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Client $client)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Client $client)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Client $client)
+    {
+        $client->delete();
+        return redirect()->route('clients.index')->with('success', 'Client supprimé.');
+    }
+}
