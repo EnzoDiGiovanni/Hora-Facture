11ce16abcc67bd78ad8686ae48f2a91cab93905e
Enzo <digiovannienzo1@gmail.com>
2025-04-16 16:49:48 +0200

    chore(routes): update web routes to support invoice and client CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\ProfileController;
+use App\Models\Invoice;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,11 +18,21 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
     Route::get('/client/create', [ClientController::class, 'create'])->name('client.create');
     Route::post('/client', [ClientController::class, 'store'])->name('client.store');
     Route::get('/clients', [ClientController::class, 'index'])->name('clients.index');
-    Route::delete('/client', [ClientController::class, 'destroy'])->name('client.destroy');
     Route::delete('/client/{client}', [ClientController::class, 'destroy'])->name('client.destroy');
+
+    Route::get('/invoice', [InvoiceController::class, 'index'])->name('invoice.index');
+    Route::post('/invoice', [InvoiceController::class, 'store'])->name('invoice.store');
+    Route::delete('/invoice/{invoice}', [InvoiceController::class, 'destroy'])->name('invoice.destroy');
+
+    Route::get('/invoices/{invoice}', [InvoiceController::class, 'show'])->name('invoice.show');
+    Route::get('/invoices/{invoice}/pdf', [InvoiceController::class, 'generatePdf'])->name('invoice.pdf');
+
+
+    Route::get('/invoice/create', [InvoiceController::class, 'create'])->name('invoice.create');
 });
 
 require __DIR__ . '/auth.php';
