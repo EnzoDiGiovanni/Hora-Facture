0c8288137795ce45ceff49e87ce2bc4a9cf0f670
Enzo <digiovannienzo1@gmail.com>
2025-04-16 16:46:12 +0200

    feat(models): add scopeOwned() for user-specific filtering

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Client extends Model
 {
-    protected $fillable = ['bussiness_name', 'email', 'ical_url'];
+    protected $fillable = ['bussiness_name', 'email', 'ical_url', 'user_id', 'hourly_rate'];
+
+    public function scopeOwned($query)
+    {
+        return $query->where('user_id', Auth::id());
+    }
 }
---
+++ app/Models/Invoice.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Auth;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth as FacadesAuth;
+
+class Invoice extends Model
+{
+    protected $fillable = ['name', 'payments_terms', 'start', 'end', 'user_id', 'client_id'];
+
+    public function lines()
+    {
+        return $this->hasMany(\App\Models\InvoiceLine::class);
+    }
+
+    public function client()
+    {
+        return $this->belongsTo(\App\Models\Client::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(\App\Models\User::class);
+    }
+
+    public function scopeOwned($query)
+    {
+        return $query->where('user_id', FacadesAuth::id());
+    }
+}
