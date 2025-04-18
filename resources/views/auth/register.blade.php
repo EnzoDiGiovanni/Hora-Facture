b8490c1166fab4ca334911af915698f99552404f
Enzo <digiovannienzo1@gmail.com>
2025-04-18 13:16:11 +0200

    feat(auth): add address input field to registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,11 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <div class="flex gap-2">
+                <x-input-label for="name" :value="__('Name')" />
+                <span class="text-red-600 items-center">*</span>
+            </div>
+
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
                 autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
@@ -12,7 +16,11 @@
 
         <!-- Email Address -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
+            <div class="flex gap-2">
+                <x-input-label for="email" :value="__('Email')" />
+                <span class="text-red-600 items-center">*</span>
+
+            </div>
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
                 autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
@@ -20,8 +28,12 @@
 
         <!-- Address -->
         <div class="mt-4">
-            <x-input-label for="address" :value="__('Address')" />
-            <x-text-input id="address" class="block mt-1 w-full" type="address" name="address" :value="old('address')"
+            <div class="flex gap-2">
+                <x-input-label for="address" :value="__('Address')" />
+                <span class="text-red-600 items-center">*</span>
+
+            </div>
+            <x-text-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')"
                 required autocomplete="username" />
             <x-input-error :messages="$errors->get('address')" class="mt-2" />
         </div>
@@ -30,7 +42,11 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <div class="flex gap-2">
+                <x-input-label for="password" :value="__('Password')" />
+                <span class="text-red-600 items-center">*</span>
+
+            </div>
 
             <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
                 autocomplete="new-password" />
@@ -40,7 +56,11 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <div class="flex gap-2">
+                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                <span class="text-red-600 items-center">*</span>
+
+            </div>
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
                 name="password_confirmation" required autocomplete="new-password" />
@@ -51,11 +71,11 @@
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                 href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+                {{ __('déjà enregistrés?') }}
             </a>
 
             <x-primary-button class="ms-4">
-                {{ __('Register') }}
+                {{ __('s\'enregistrer') }}
             </x-primary-button>
         </div>
     </form>
